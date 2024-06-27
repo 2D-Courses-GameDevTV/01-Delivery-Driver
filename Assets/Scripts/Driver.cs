40313e5a241d8c0de2f60156245f90f2bd14330e
Eduard Barsan <eduard.barsan.gmp@gmail.com>
2024-06-27 21:50:19 +0200

    Boost and slow added

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -7,6 +7,9 @@ public class Driver : MonoBehaviour
 
     [SerializeField] float steerSpeed = 200f;
     [SerializeField] float moveSpeed = 20f;
+    [SerializeField] float slowSpeed = 10f;
+    [SerializeField] float boostSpeed = 30f;
+    [SerializeField] string actualSpeed = "Normal";
  
     void Start()
     {
@@ -20,8 +23,26 @@ public class Driver : MonoBehaviour
         transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0, moveAmount, 0);
 
-        
-        
+      
         
     }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+
+        if (other.tag == "Boost")
+        {
+            moveSpeed = boostSpeed;
+            actualSpeed = "Fast";
+        }
+
+    }
+
+   void OnCollisionEnter2D(Collision2D collision)
+    {
+        moveSpeed = slowSpeed;
+    }
+
+
 }
